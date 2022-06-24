28cdb856a696e027c33f6787e81bc1ba9b83c2dd
Sanja Kovacic <sanjavus@gmail.com>
2022-06-24 15:09:46 +0200

    Scroll message

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,6 +1,14 @@
 import "./Messages.css";
+import React, { useEffect, useRef } from 'react';
 
 const Messages = (props) => {
+
+  const scrollDown = useRef(null);
+
+  useEffect(() => {
+    scrollDown.current?.scrollIntoView({ behaviour: "smooth" });
+  }, [props]);
+
   const renderMessage = (message) => {
     const { member, data, id } = message;
     const messageFromMe = member.id === props.currentMember.id;
@@ -9,20 +17,20 @@ const Messages = (props) => {
       : "messages--message";
 
 
+    return (
+
+      <li className={className} key={id}>
+        <div className="info--content">
+          <span className="username">{member.clientData.username}</span>
+          <div className="avatar" />
+        </div>
+        <div className="message--content" ref={scrollDown}>
+          <p className="text">{data}</p>
+        </div>
+      </li>
+    );
+  };
   return (
-  
-    <li className={className} key={id}>
-      <div className="info--content">
-        <span className="username">{member.clientData.username}</span>
-        <div className="avatar" />
-      </div>
-      <div className="message--content">
-        <p className="text">{data}</p>
-      </div>
-    </li>
-  );
-};
-return (
     <div className="App-chat-page--messages-field">
       <ul className="messages--list">
         {props.messages.map((item) => renderMessage(item))}
